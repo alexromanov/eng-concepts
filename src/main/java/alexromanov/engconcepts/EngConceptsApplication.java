574bd17e210744ae11ea80a64d7db15b2b0745d6
alexromanov <al8x.romanov@gmail.com>
2020-04-27 10:14:54 +0300

    Add empty spring boot project

---
+++ src/main/java/alexromanov/engconcepts/EngConceptsApplication.java
@@ -0,0 +1,13 @@
+package alexromanov.engconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngConceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngConceptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alexromanov/engconcepts/EngConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package alexromanov.engconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
