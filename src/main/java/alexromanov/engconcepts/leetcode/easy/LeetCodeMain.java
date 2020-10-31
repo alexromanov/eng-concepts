6e11b479cc1dfe9b1f26bea09b27ecafcde050ea
Oleksandr Romanov <al8x.romanov@gmail.com>
2020-10-31 20:16:16 +0200

    Add leetcode - 1108, 1342, 1281

--- src/main/java/alexromanov/engconcepts/leetcode/easy/LeetCodeMain.java
+++ src/main/java/alexromanov/engconcepts/leetcode/easy/LeetCodeMain.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeetCodeMain {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
     }
 
     /**
+     * 1480
      * https://leetcode.com/problems/running-sum-of-1d-array
-     * @param nums
-     * @return
      */
     public int[] runningSum(int[] nums) {
         int[] res = new int[nums.length];
         res[0] = nums[0];
         int sum = nums[0];
-        for (int i = 1; i < nums.length; i++){
+        for (int i = 1; i < nums.length; i++) {
             sum += nums[i];
             res[i] = sum;
         }
@@ -25,16 +24,14 @@ public class LeetCodeMain {
     }
 
     /**
+     * 1431
      * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
-     * @param candies
-     * @param extraCandies
-     * @return
      */
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> res = new ArrayList<>();
         int max = candies[0];
-        for (int i = 1; i < candies.length; i++){
-            if (candies[i] > max){
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max) {
                 max = candies[i];
             }
         }
@@ -49,17 +46,15 @@ public class LeetCodeMain {
     }
 
     /**
+     * 1470
      * https://leetcode.com/problems/shuffle-the-array/
-     * @param nums
-     * @param n
-     * @return
      */
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[nums.length];
         int i = 0;
         int j = n;
         int c = 0;
-        while (i != n){
+        while (i != n) {
             result[c] = nums[i];
             c++;
             result[c] = nums[j];
@@ -68,5 +63,51 @@ public class LeetCodeMain {
             j++;
         }
         return result;
+    }
+
+    /**
+     * 1108
+     * https://leetcode.com/problems/defanging-an-ip-address/
+     */
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+
+    /**
+     * 1342
+     * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+     */
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        if (num == 0) {
+            return steps;
+        } else {
+            while (num != 0) {
+                if (num % 2 == 0) {
+                    num = num / 2;
+                } else {
+                    num -= 1;
+                }
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    /**
+     * 1281
+     * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+     */
+    public int subtractProductAndSum(int n) {
+        int pr = 1;
+        int sum = 0;
+        int i;
+        while (n > 0) {
+            i = n % 10;
+            pr *= i;
+            sum += i;
+            n = n / 10;
+        }
+        return pr - sum;
     }
 }
