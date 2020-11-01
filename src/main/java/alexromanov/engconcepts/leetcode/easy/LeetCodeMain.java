8e83ba64f3526513233a8c77574584b164e7b605
Oleksandr Romanov <al8x.romanov@gmail.com>
2020-11-01 11:28:08 +0200

    Add leetcode - 1, 167, 1450, restoreString

--- src/main/java/alexromanov/engconcepts/leetcode/easy/LeetCodeMain.java
+++ src/main/java/alexromanov/engconcepts/leetcode/easy/LeetCodeMain.java
@@ -1,16 +1,14 @@
 package alexromanov.engconcepts.leetcode.easy;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class LeetCodeMain {
 
     public static void main(String[] args) {
-        String a = "HellO";
-        System.out.println(toLowerCase(a));
+
     }
 
     /**
@@ -133,10 +131,10 @@ public class LeetCodeMain {
     /**
      * 709 - https://leetcode.com/problems/to-lower-case/
      */
-    public static String toLowerCase(String str) {
+    public String toLowerCase(String str) {
         char[] orig = str.toCharArray();
         int c;
-        for (int i = 0; i < orig.length; i++){
+        for (int i = 0; i < orig.length; i++) {
             c = orig[i];
             if (c >= 65 & c <= 90) {
                 c += 32;
@@ -144,5 +142,59 @@ public class LeetCodeMain {
             }
         }
         return new String(orig);
+    }
+
+    /**
+     * 1450 - https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+     */
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int res = 0;
+        int i = 0;
+        while (i < startTime.length) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+                res++;
+            }
+            i++;
+        }
+        return res;
+    }
+
+
+    public static String restoreString(String s, int[] indices) {
+        char[] res = new char[s.length()];
+        for (int index : indices) {
+            res[index] = s.charAt(index);
+        }
+        return new String(res);
+    }
+
+    // 1 - https://leetcode.com/problems/two-sum/
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int e = nums[i];
+            if (map.containsKey(target - e)) {
+                return new int[] {map.get(target - e), i};
+            } else {
+                map.put(e, i);
+            }
+        }
+        return null;
+    }
+
+    // 167 - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+    public int[] twoSum2(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int a = numbers[i] + numbers[j];
+            if (a > target) {
+                j--;
+            } else if (a < target) {
+                i++;
+            } else {
+                return new int[] {i + 1, j + 1};
+            }
+        }
+        return null;
     }
 }
